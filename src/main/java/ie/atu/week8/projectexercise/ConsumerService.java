e4d8238a6e77a349cf47748453861ed25bb2925c
G00405094@atu.ie <G00405094@atu.ie>
2024-11-18 16:59:53 +0000

    Tested turning consumer on and off

--- src/main/java/ie/atu/week8/projectexercise/ConsumerService.java
+++ src/main/java/ie/atu/week8/projectexercise/ConsumerService.java
@@ -1,9 +1,11 @@
 package ie.atu.week8.projectexercise;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnProperty(name = "consumer.enabled", havingValue = "true")
 public class ConsumerService {
 
     @RabbitListener(queues = "productQueue")
